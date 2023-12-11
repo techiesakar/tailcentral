abc5bdf0fc88ad69ef959c75650f2e6edbd0966f
Sakar Aryal <techiesakar@gmail.com>
2023-12-11 18:49:57 +0545

    db changed

--- components/modals/add-block-modal.tsx
+++ components/modals/add-block-modal.tsx
@@ -2,7 +2,7 @@
 import { z } from "zod";
 import axios from "axios";
 import { useModal } from "@/hooks/use-modal-store";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
