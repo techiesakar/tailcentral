8d77cfbdd83675e6a847abe64a25a9341c01cdde
Sakar Aryal <techiesakar@gmail.com>
2023-12-21 17:35:17 +0545

    feat: uploadthing added

---
+++ app/(routes)/add/_components/component-form.tsx
@@ -0,0 +1,212 @@
+"use client";
+import { z } from "zod";
+import { useEffect, useState } from "react";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+
+import { createComponent, findAllBlocks } from "@/app/action";
+import { ComponentSchema } from "@/types/schema";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import FileUpload from "@/components/file-upload";
+
+
+type BlockItemType = {
+    id: string;
+    title: string;
+    slug: string;
+};
+
+
+export default function ComponentForm() {
+    const [imageUrl, setImageUrl] = useState("")
+
+    const router = useRouter();
+
+    const [blocks, setAllBlocks] = useState<BlockItemType[]>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const fetchAllBlocks = await findAllBlocks();
+                setAllBlocks(fetchAllBlocks);
+            } catch (error) {
+                console.error(error);
+                setAllBlocks([]);
+            }
+        };
+
+        fetchData()
+    }, []);
+
+
+
+    const form = useForm<z.infer<typeof ComponentSchema>>({
+        resolver: zodResolver(ComponentSchema),
+        defaultValues: {
+            title: "",
+            code: "",
+            blockSlug: "",
+            darkThumb: "",
+            lightThumb: ""
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof ComponentSchema>) => {
+        try {
+            await createComponent(values);
+            router.refresh();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <Form {...form}>
+            {imageUrl.length >= 6 && <img src={imageUrl} alt="hello" height={400} width={400} className="h-10 w-10" />}
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4 mt-3"
+            >
+                <div className="w-full flex md:flex-row flex-col gap-6 items-center">
+
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormLabel>Title</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Component Name"
+                                        {...field}
+                                        onChange={(e) => {
+                                            field.onChange(e);
+                                        }}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="blockSlug"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormLabel>Block</FormLabel>
+                                <FormControl>
+                                    <Select
+                                        onValueChange={field.onChange}
+                                        defaultValue={field.value}
+                                    >
+                                        <SelectTrigger className="w-[280px]">
+                                            <SelectValue
+                                                placeholder="Select a Block"
+                                                className="capitalize"
+                                            />
+                                        </SelectTrigger>
+                                        <SelectContent className="capitalize">
+                                            <SelectGroup>
+                                                <SelectLabel>Blocks</SelectLabel>
+
+                                                {blocks.map((item) => (
+                                                    <SelectItem
+                                                        key={item.slug}
+                                                        value={item.slug}
+                                                        className="capitalize"
+                                                    >
+                                                        {item.title}
+                                                    </SelectItem>
+                                                ))}
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+
+                <div className="w-full flex gap-6">
+                    <FormField
+                        control={form.control}
+                        name="lightThumb"
+                        render={({ field }) => (
+                            <FormItem>
+                                <div className="grid w-full max-w-sm items-center gap-1.5">
+                                    <FormLabel>Light Image</FormLabel>
+                                    <FormControl>
+
+                                        <FileUpload
+                                            endpoint="componentThumb"
+                                            value={field.value}
+                                            onChange={field.onChange}
+                                        />
+                                    </FormControl>
+                                </div>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="darkThumb"
+                        render={({ field }) => (
+                            <FormItem>
+                                <div className="grid w-full max-w-sm items-center gap-1.5">
+                                    <FormLabel>Dark Image</FormLabel>
+                                    <FileUpload
+                                        endpoint="componentThumb"
+                                        value={field.value}
+                                        onChange={field.onChange}
+                                    />
+                                </div>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <FormField
+                    control={form.control}
+                    name="code"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Code</FormLabel>
+                            <FormControl>
+                                <Textarea
+                                    className="min-h-[400px]"
+                                    placeholder="Component Name"
+                                    {...field}
+                                    onChange={(e) => {
+                                        field.onChange(e);
+                                    }}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
---
+++ components/file-upload.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Image from "next/image";
+
+import { X } from "lucide-react";
+import "@uploadthing/react/styles.css";
+import { UploadDropzone } from "@/app/utils/uploadthing";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "componentThumb";
+}
+const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(`ERROR! ${error.message}`);
+      }}
+    />
+  );
+};
+
+export default FileUpload;
