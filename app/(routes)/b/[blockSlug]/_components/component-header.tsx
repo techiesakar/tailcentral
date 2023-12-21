299e0acffc62aca1c69410018a90443681d19f39
Sakar Aryal <techiesakar@gmail.com>
2023-12-21 18:34:36 +0545

    fix: component css

--- app/(routes)/b/[blockSlug]/_components/component-header.tsx
+++ app/(routes)/b/[blockSlug]/_components/component-header.tsx
@@ -1,8 +1,6 @@
 "use client";
-import { convertHtmlToJsx } from "@/hooks/use-html-jsx";
-import { Check, Code, Copy, Cross, Eye, X } from "lucide-react";
-import Link from "next/link";
-import { useParams, usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Check, Code, Copy, Eye, X } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { FaReact } from "react-icons/fa";
 
@@ -12,7 +10,7 @@ type PropsType = {
   blockSlug: string;
   jsx?: string
 };
-const ComponentHeader = ({ title, code, blockSlug, jsx }: PropsType) => {
+const ComponentHeader = ({ code, blockSlug, jsx }: PropsType) => {
   const router = useRouter()
   const [copied, setCopied] = useState(false);
   const pathname = usePathname();
@@ -37,7 +35,7 @@ const ComponentHeader = ({ title, code, blockSlug, jsx }: PropsType) => {
 
   return (
     <header className=" bg-gray-100 h-14 border-b">
-      <div className="bottom border-x-0 border-b-0  flex items-center justify-between border h-14 px-6">
+      <div className="bottom border-x-0 border-b-0  flex items-center justify-between border h-14 pl-6">
         <h1 >{modeType === "preview" && "Preview"} {modeType === "jsx" && "JSX"} {modeType === "html" && "HTML"}</h1>
         <div className="flex items-center">
           <Eye onClick={() => router.push(`/b/${blockSlug}/` + componentId + "?mode=preview")} className="h-14 border-l text-gray-400 cursor-pointer hover:text-indigo-500 transition-all duration-300 w-14  p-4" />
@@ -53,7 +51,7 @@ const ComponentHeader = ({ title, code, blockSlug, jsx }: PropsType) => {
           )}
 
 
-          <X onClick={() => router.push(`/b/${blockSlug}`)} className="h-14 border-l text-gray-400 cursor-pointer  hover:text-indigo-500 transition-all duration-300 w-14  p-4" />
+          <X onClick={() => router.push(`/b/${blockSlug}`)} className="h-14 border-x text-gray-400 cursor-pointer  hover:text-indigo-500 transition-all duration-300 w-14  p-4" />
 
         </div>
       </div>
